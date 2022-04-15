5d5ff659cfb517f9502b0628ed6c7f16a896dbd7
HubertGoral <hg@visla.co>
2022-04-15 11:56:01 +0200

    add tests and loader

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,11 @@ const url = 'https://anapioficeandfire.com/api';
 export const fetchCharacters = async (page: number, size: number) => {
     return axios.get(
         `${url}/characters?page=${page}&pageSize=${size}`
-    )
+    ).catch((error) => {
+        if (error.response) {
+            alert(error);
+        }
+    })
 }
 
 export const fetchHouses = async (id: string) => {
